7c4a44a38a5687b0cb9de5658aef49aa1a0a3eae
jinahkwak <wlsdk8949@naver.com>
2020-08-27 23:52:51 +0900

    ebook top100

---
+++ web_crawling/ebook_top100/openpyxl.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+import openpyxl
+
+wb = openpyxl.Workbook()
+sheet = wb.active
+sheet.append(["제목","저자"])
+
+for p in range(1, 6):
+    raw = requests.get("https://series.naver.com/ebook/top100List.nhn?page="+str(p),
+                       headers={"User-Agent":"Mozilla/5.0"})
+    html = BeautifulSoup(raw.text, 'html.parser')
+    container = html.select("div.lst_thum_wrap li")
+
+
+    for cont in container:
+        title = cont.select_one("strong").text
+        writer = cont.select_one("span.writer").text
+
+        print(title, writer)
+        sheet.append([title, writer])
+
+        # 열사이 쉼표제거
+        title = title.replace(",", "")
+        writer = writer.replace(",", "")
+
+wb.save("ebook.xlsx")

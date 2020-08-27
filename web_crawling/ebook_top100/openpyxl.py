25873ee5c3e51bace5e8cc2a39263d7627faf89d
jinahkwak <wlsdk8949@naver.com>
2020-08-28 00:10:08 +0900

    add selector

--- web_crawling/ebook_top100/openpyxl.py
+++ web_crawling/ebook_top100/openpyxl.py
@@ -10,11 +10,13 @@ for p in range(1, 6):
     raw = requests.get("https://series.naver.com/ebook/top100List.nhn?page="+str(p),
                        headers={"User-Agent":"Mozilla/5.0"})
     html = BeautifulSoup(raw.text, 'html.parser')
+    # 컨테이너 선택자: 전체 > 자식
     container = html.select("div.lst_thum_wrap li")
 
 
     for cont in container:
-        title = cont.select_one("strong").text
+        # 제목 선택자: strong / a strong
+        title = cont.select_one("a strong").text
         writer = cont.select_one("span.writer").text
 
         print(title, writer)
